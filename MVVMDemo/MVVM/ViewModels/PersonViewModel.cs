1d30b02116bff3ef1b484e2b382ac9622fff3527
Msluka <msluka.info@gmail.com>
2023-01-18 23:44:46 +0100

    Add project files.

---
+++ MVVMDemo/MVVM/ViewModels/PersonViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MVVMDemo.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMDemo.MVVM.ViewModels
+{
+    public class PersonViewModel
+    {
+        public Person Person { get; set; }
+        public string Test { get; set; }
+
+        public PersonViewModel() {
+
+            Test = $"Custom format of Date:\n {new DateTime(2000, 08, 05).ToString("yyyy-MM-dd")}";          
+
+            Person = new Person
+            {
+                Name = "Hector",
+                Age = 30,
+                Married = true,
+                BirthDate = new DateTime(2000, 08, 05),               
+                Weight = 70,
+                Lunchtime = new TimeSpan(10,0,0)
+            };
+
+        }
+    }
+}
